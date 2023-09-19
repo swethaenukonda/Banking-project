76223560cb900a72b92f188b4aad69d31cda94b5
swethaenukonda <136258931+swethaenukonda@users.noreply.github.com>
2023-09-19 13:54:37 +0530

    Create main.tf

---
+++ my-serverfiles/main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "test-server" {
+  ami           = "ami-0f5ee92e2d63afc18" 
+  instance_type = "t2.micro" 
+  key_name = "BabucKeypair"
+  vpc_security_group_ids= ["sg-00f7cae4b48423f8c"]
+  connection {
+    type     = "ssh"
+    user     = "ubuntu"
+    private_key = file("./BabucKeypair.pem")
+    host     = self.public_ip
+  }
+  provisioner "remote-exec" {
+    inline = [ "echo 'wait to start instance' "]
+  }
+  tags = {
+    Name = "test-server"
+  }
+  provisioner "local-exec" {
+        command = " echo ${aws_instance.test-server.public_ip} > inventory "
+  }
+   provisioner "local-exec" {
+  command = "ansible-playbook /var/lib/jenkins/workspace/Banking/my-serverfiles/finance-playbook.yml "
+  } 
+}
