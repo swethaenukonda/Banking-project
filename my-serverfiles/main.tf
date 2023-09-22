bc383c7f5fccdb1f8f5d3b8d8c95f3cd96b5c929
swethaenukonda <136258931+swethaenukonda@users.noreply.github.com>
2023-09-22 20:13:54 +0530

    Update main.tf

--- my-serverfiles/main.tf
+++ my-serverfiles/main.tf
@@ -19,6 +19,6 @@ resource "aws_instance" "test-server" {
         command = " echo ${aws_instance.test-server.public_ip} > inventory "
   }
    provisioner "local-exec" {
-  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-project/my-serverfiles/banking playbook.yml"
+  command = "ansible-playbook /var/lib/jenkins/workspace/Banking-project/my-serverfiles/banking-playbook.yml"
   } 
 }
